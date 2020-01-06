a1dadb8f226d79502d62f7015e317ce5caae4672
dinutharaLakshitha <dinuthara.15@cse.mrt.ac.lk>
2020-01-06 18:02:37 +0530

    location added to firebase

--- LetsChat/app/src/main/java/com/example/singhkshitiz/letschat/MainActivity.java
+++ LetsChat/app/src/main/java/com/example/singhkshitiz/letschat/MainActivity.java
@@ -1,21 +1,39 @@
 package com.example.singhkshitiz.letschat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+
 import com.example.singhkshitiz.letschat.Fragments.MyFragmentPagerAdapter;
+//import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,32 +41,167 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mauth;
     ViewPager mviewPager;
     MyFragmentPagerAdapter mFragmentPagerAdapter;
     TabLayout mtabLayout;
     DatabaseReference mDatabaseReference;
+    //    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+    private static final String TAG = "MyActivity";
+    Location currentLocation;
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
     //Toolbar mtoolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mauth=FirebaseAuth.getInstance();
+        mauth = FirebaseAuth.getInstance();
 
-        mviewPager=(ViewPager)findViewById(R.id.viewPager);
+        mviewPager = (ViewPager) findViewById(R.id.viewPager);
 
         //---ADDING ADAPTER FOR FRAGMENTS IN VIEW PAGER----
-        mFragmentPagerAdapter=new MyFragmentPagerAdapter(getSupportFragmentManager());
+        mFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
         mviewPager.setAdapter(mFragmentPagerAdapter);
 
         //---SETTING TAB LAYOUT WITH VIEW PAGER
-        mtabLayout=(TabLayout)findViewById(R.id.tabLayout);
+        mtabLayout = (TabLayout) findViewById(R.id.tabLayout);
         mtabLayout.setupWithViewPager(mviewPager);
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//        fetchLastLocation();
+        startLocationUpdates();
     }
+
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        // do work here
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+//        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        Log.i("MainActivity", String.valueOf(location.getLatitude()));
+
+        FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+        if(current_user!=null){
+            final String uid=current_user.getUid();
+            Map userMap=new HashMap();
+            userMap.put("Latitude",location.getLatitude());
+            userMap.put("Longitude",location.getLongitude());
+            mDatabaseReference.child(uid).updateChildren(userMap);
+        }
+
+    }
+
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // GPS location can be null if GPS is switched off
+                        if (location != null) {
+                            onLocationChanged(location);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+
+//    private void fetchLastLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]
+//                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+//            return;
+//        }
+//        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                if (location != null) {
+//                    currentLocation = location;
+////                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()
+////                            + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+////                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+////                            .findFragmentById(R.id.map);
+////                    mapFragment.getMapAsync(MainActivity.this);
+//                    Log.i(TAG, "Latitude" + currentLocation.getLatitude());
+//                    Log.i(TAG, "Longitude" + currentLocation.getLongitude());
+//
+//                }
+//            }
+//        });
+//    }
 
     //----SHOWING ALERT DIALOG FOR EXITING THE APP----
     @Override
